07757007444f48308ce8faf6410edc10747256e7
Erlang Parasu <erlangparasu@gmail.com>
2025-09-13 11:43:11 +0800

    fix: bump version

--- build_push_image_shipping-service.sh
+++ build_push_image_shipping-service.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-docker build . --tag docker.io/erlangp/shipping-service:latest
+docker build . --tag docker.io/erlangp/shipping-service:1.0.1
 echo "$PASSWORD_DOCKER_HUB" | docker login --username erlangp --password-stdin
-docker push docker.io/erlangp/shipping-service:latest
+docker push docker.io/erlangp/shipping-service:1.0.1
